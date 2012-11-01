ad3c33a4009b85765170cd562b67d3dc6b18ba46
Frederic Lepied <flepied@gmail.com>
2012-11-01 21:18:27 +0100

    adminkit.py: refactored finalize by breaking it in small functions

--- adminkit.py
+++ adminkit.py
@@ -192,26 +192,184 @@ def global_conf(conf):
         
     return res
 
-def expand_variables(str, plan):
+def expand_variables(str_, plan):
     """Expand variable in the string using jinja2."""
-    return _ENV.from_string(str).render(plan.var)
+    return _ENV.from_string(str_).render(plan.var)
 
-def finalize(plan):
-    """Do the actual action that were registered for the host."""
+def manage_pkgs(plan, system):
+    """Manage system packages."""
+    if len(plan.pkg) > 0:
+        installed_pkgs = system.get_packages()
+        for p in plan.pkg:
+            if p in installed_pkgs:
+                if _DEBUG:
+                    print p, 'already installed'
+            else:
+                status, output = system.install_package(p)
+                if status == 0:
+                    print 'installed package', p
+                else:
+                    print 'problems installing package', p, ':'
+                    print output
+                    return 1
+    return 0
 
-    global _ENV
-    global _RET
+def manage_dirs(plan):
+    """Manage directory creation."""
+    for path in plan.get_dirs():
+        d = expand_variables(os.path.join(_DEST, path[1:]), plan)
+        if not os.path.isdir(d):
+            print 'creating directory', d
+            os.makedirs(d)
+        else:
+            if _DEBUG:
+                print d, 'already exists'
+    return 0
+
+def manage_files(plan, strings):
+    """Manage the creation of files from templates."""
+    ret = 0
+    check_vars(plan.var, _VARS_FILE)
+    modified = []
+    for f in plan.get_files():
+        uid = -1
+        gid = -1
+        if type(f) == STRING_TYPE:
+            mode = 0644
+        else:
+            mode = f[1]
+            if len(f) >= 3 and f[2] != None:
+                try:
+                    uid = pwd.getpwnam(f[2]).pw_uid
+                except KeyError:
+                    print 'Unable to find uid for', f[2]
+                    ret = 1
+            if len(f) >= 4 and f[3] != None:
+                try:
+                    gid = grp.getgrnam(f[3]).gr_gid
+                except KeyError:
+                    print 'Unable to find gid for', f[3]
+                    ret = 1
+            f = f[0]
+                
+        l = find_file_with_vars(f, os.path.join(_ROOT, 'files'), strings + plan.get_roles())
+        if l:
+            t = expand_variables(os.path.join(_DEST, f[1:]), plan)
+            if is_newer(l, t) or is_newer(_VARS_FILE, t):
+                if copyfile(l, t, plan.var, mode, uid, gid):
+                    modified.append(f)
+                    print 'copied', l, 'to', t
+                else:
+                    print 'touched', t
+            else:
+                if _DEBUG:
+                    print l, 'not newer than', t
+        else:
+            print 'ERROR', f, 'not found'
+            ret = 1
+    return ret, modified
+
+def manage_perms(plan):
+    """Manage permissions of files and directories."""
+    ret = 0
+    for f in plan.get_perms():
+        try:
+            s = os.stat(f[0])
+            if s[0] & 07777 != f[1]:
+                print 'Changing mode of %s from 0%o to 0%o' % (f[0], s[0] & 07777, f[1])
+                os.chmod(f[0], f[1])
+        except:
+            print 'ERROR changing perms of', f[0], ':'
+            print sys.exc_info()[1]
+            ret = 1
+    return ret
+
+def manage_services(plan, modified):
+    """Manage services."""
+    ret = 0
     
-    _RET = 0                             # return value
-    
-    for s in (plan.get_var('oscode'), plan.get_var('osname')):
+    reloaded = []
+    for f in modified:
+        try:
+            for s in plan.get_service(f):
+                if s not in reloaded:
+                    reloaded.append(s)
+                    print 'reloading service', s
+                    status, output = commands.getstatusoutput('/etc/init.d/%s reload || /etc/init.d/%s restart' % (s, s))
+                    if status != 0:
+                        print 'Error reloading %s:' % s
+                        print output
+                        ret = 1
+            for r in plan.command.keys():
+                if r.search(f):
+                    cmd = plan.get_command(r)
+                    print 'launching command', s, 'for', f
+                    status, output = commands.getstatusoutput(cmd)
+                    if status != 0:
+                        print 'Error reloading %s:' % cmd
+                        print output
+                        ret = 1
+        except KeyError:
+            pass
+
+    return ret
+
+def manage_pidfiles(plan):
+    """Manage pid files to check that services are present else relaunch them."""
+    ret = 0
+    for p in plan.pidfile.keys():
+        restart = False
+        if not os.path.exists(plan.get_pidfile(p)):
+            restart = False
+        else:
+            fd = open(plan.get_pidfile(p))
+            content = fd.read(-1)
+            fd.close()
+            if content[-1] == '\n':
+                content = content[:-1]
+            if not os.path.exists('/proc/' + content):
+                restart = True
+        if restart:
+            print 'Restarting service', p
+            status, output = commands.getstatusoutput('/etc/init.d/%s restart' % (p,))
+            if status != 0:
+                print 'Error restarting %s:' % p
+                print output
+                ret = 1
+    return ret
+
+def manage_onces(plan):
+    """Manage commands that needs to be launched only once."""
+    ret = 0
+    for cmd in plan.get_onces():
+        hsh = hashlib.sha1(cmd).hexdigest()
+        path = os.path.join(_ONCE_DIR, hsh)
+        if not os.path.exists(path):
+            print 'Running once', cmd
+            status, output = commands.getstatusoutput(cmd)
+            if status == 0:
+                open(path, 'w').close()
+            else:
+                print 'Error running once %s:' % cmd
+                print output
+                ret = 1
+    return ret
+
+def init_system(plan):
+    """Initialize the system object according to the OS detection."""
+    for s in (plan.get_var('oscode'), plan.get_var('osname'), 'generic'):
         try:
             mod = __import__(s)
             break
         except ImportError:
             continue
-    system = mod.System()
-    # Loading roles
+    return mod.System()
+
+def init_roles(plan):
+    """Run role files to fill the plan."""
+    global _ENV
+
+    ret = 0
     path = []    
     strings = []
     for p in plan.var.values() + plan.get_roles() + ['']:
@@ -256,7 +414,7 @@ def finalize(plan):
             execfile(f, functions)
         else:
             print 'No such role', c, 'in', ':'.join(path)
-            _RET = 1
+            ret = 1
     
     if _DEBUG:
         print _ENV.from_string('hostname is {{ hostname }}').render(plan.var)
@@ -266,139 +424,31 @@ def finalize(plan):
         print 'ONCE', plan.get_onces()
         print 'PKGS', plan.get_pkgs()
 
-    # manage pkgs
-    if len(plan.pkg) > 0:
-        installed_pkgs = system.get_packages()
-        for p in plan.pkg:
-            if p in installed_pkgs:
-                if _DEBUG:
-                    print p, 'already installed'
-            else:
-                status, output = system.install_package(p)
-                if status == 0:
-                    print 'installed package', p
-                else:
-                    print 'problems installing package', p, ':'
-                    print output
-                    _RET = 1
+    return ret, path, strings
 
-    # Managing directories
-    for path in plan.get_dirs():
-        d = expand_variables(os.path.join(_DEST, path[1:]), plan)
-        if not os.path.isdir(d):
-            print 'creating directory', d
-            os.makedirs(d)
-        else:
-            if _DEBUG:
-                print d, 'already exists'
-            
-    # Managing files
-    check_vars(plan.var, _VARS_FILE)
-    modified = []
-    for f in plan.get_files():
-        uid = -1
-        gid = -1
-        if type(f) == STRING_TYPE:
-            mode = 0644
-        else:
-            mode = f[1]
-            if len(f) >= 3 and f[2] != None:
-                try:
-                    uid = pwd.getpwnam(f[2]).pw_uid
-                except KeyError:
-                    print 'Unable to find uid for', f[2]
-                    _RET = 1
-            if len(f) >= 4 and f[3] != None:
-                try:
-                    gid = grp.getgrnam(f[3]).gr_gid
-                except KeyError:
-                    print 'Unable to find gid for', f[3]
-                    _RET = 1
-            f = f[0]
-                
-        l = find_file_with_vars(f, os.path.join(_ROOT, 'files'), strings + plan.get_roles())
-        if l:
-            t = expand_variables(os.path.join(_DEST, f[1:]), plan)
-            if is_newer(l, t) or is_newer(_VARS_FILE, t):
-                if copyfile(l, t, plan.var, mode, uid, gid):
-                    modified.append(f)
-                    print 'copied', l, 'to', t
-                else:
-                    print 'touched', t
-            else:
-                if _DEBUG:
-                    print l, 'not newer than', t
-        else:
-            print 'ERROR', f, 'not found'
-            _RET = 1
+def finalize(plan):
+    """Do the actual action that were registered for the host."""
+    
+    system = init_system(plan)
+    
+    ret, path, strings = init_roles(plan)
+    
+    ret += manage_pkgs(plan, system)
 
-    for f in plan.get_perms():
-        try:
-            s = os.stat(f[0])
-            if s[0] & 07777 != f[1]:
-                print 'Changing mode of %s from 0%o to 0%o' % (f[0], s[0] & 07777, f[1])
-                os.chmod(f[0], f[1])
-        except:
-            print 'ERROR changing perms of', f[0], ':'
-            print sys.exc_info()[1]
-            _RET = 1
-    # Managing services
-    reloaded = []
-    for f in modified:
-        try:
-            for s in plan.get_service(f):
-                if s not in reloaded:
-                    reloaded.append(s)
-                    print 'reloading service', s
-                    status, output = commands.getstatusoutput('/etc/init.d/%s reload || /etc/init.d/%s restart' % (s, s))
-                    if status != 0:
-                        print 'Error reloading %s:' % s
-                        print output
-                        _RET = 1
-            for r in plan.command.keys():
-                if r.search(f):
-                    cmd = plan.get_command(r)
-                    print 'launching command', s, 'for', f
-                    status, output = commands.getstatusoutput(cmd)
-                    if status != 0:
-                        print 'Error reloading %s:' % cmd
-                        print output
-                        _RET = 1
-        except KeyError:
-            pass
+    ret += manage_dirs(plan)
+    
+    ret2, modified = manage_files(plan, strings)
+    ret += ret2
 
-    for p in plan.pidfile.keys():
-        restart = False
-        if not os.path.exists(plan.get_pidfile(p)):
-            restart = False
-        else:
-            fd = open(plan.get_pidfile(p))
-            content = fd.read(-1)
-            fd.close()
-            if content[-1] == '\n':
-                content = content[:-1]
-            if not os.path.exists('/proc/' + content):
-                restart = True
-        if restart:
-            print 'Restarting service', p
-            status, output = commands.getstatusoutput('/etc/init.d/%s restart' % (p,))
-            if status != 0:
-                print 'Error restarting %s:' % p
-                print output
-                _RET = 1
-    for cmd in plan.get_onces():
-        hsh = hashlib.sha1(cmd).hexdigest()
-        path = os.path.join(_ONCE_DIR, hsh)
-        if not os.path.exists(path):
-            print 'Running once', cmd
-            status, output = commands.getstatusoutput(cmd)
-            if status == 0:
-                open(path, 'w').close()
-            else:
-                print 'Error running once %s:' % cmd
-                print output
-                _RET = 1
-    return _RET
+    ret += manage_perms(plan)
+
+    ret += manage_services(plan, modified)
+
+    ret += manage_pidfiles(plan)
+
+    ret += manage_onces(plan)
+    
+    return ret
 
 def set_root(rt):
     """Initialize global variables."""
